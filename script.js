a15a505f46287a67b11d6b650c7899c5de02e5ec
In Ha Cho <ihcho1211@gmail.com>
2026-03-23 16:41:46 -0400

    Initial

---
+++ script.js
@@ -0,0 +1,19 @@
+const reveals = document.querySelectorAll(".reveal");
+
+const observer = new IntersectionObserver(
+  (entries) => {
+    for (const entry of entries) {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("is-visible");
+        observer.unobserve(entry.target);
+      }
+    }
+  },
+  {
+    threshold: 0.14,
+  },
+);
+
+for (const item of reveals) {
+  observer.observe(item);
+}
